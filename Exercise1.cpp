d8faec47a676944417ec4903b0ea5cd978f12c08
Nguyễn Quí Vinh Quang <60070577+nguyenquivinhquang@users.noreply.github.com>
2020-08-12 12:11:43 +0700

    Update Exercise1.cpp

--- Exercise1.cpp
+++ Exercise1.cpp
@@ -6,21 +6,34 @@
 |________________________________________________|
 */
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<string.h>
-#include<limits.h>
-#define SIZE 1000
+#include <bits/stdc++.h>
+#define N 100
+#define ll long long
+#define pp pair<int, int>
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define reset(x) memset(x, 0, sizeof(x))
 
-void ex1(char *str[],int n){
-	//your codes here
+using namespace std;
+
+void ex1(string str[], int n)
+{
+    string s = str[0];
+    for (int j = 1; j < n; j++)
+    {
+        string temp = "";
+        for (int i = 0; i < min(s.length(), str[j].length()); i++)
+            if (s[i] == str[j][i])
+                temp = temp + s[i];
+            else
+                break;
+        s = temp;
+    }
+    cout << s;
 }
+int main()
+{
+    string str[4] = {"abc", "abcd", "abe", "abdr"};
+    ex1(str, 4);
 
-int main(int argc, char *argv[]) {
-	
-	char *str[]={"abc","abcd","abe","abdr"};
-	ex1(str,4);
-			
-	return 0;
+    return 0;
 }
