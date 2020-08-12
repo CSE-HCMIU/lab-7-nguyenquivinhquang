329f3e331e4e5439a0cb3567cfd38b39347c12f1
Nguyễn Quí Vinh Quang <60070577+nguyenquivinhquang@users.noreply.github.com>
2020-08-12 12:20:06 +0700

    Update Exercise4.cpp

--- Exercise4.cpp
+++ Exercise4.cpp
@@ -1,27 +1,36 @@
-/*
-4. Input 2 binary number using string then return the sum of those two.
-Result should use string.
-Ex:
- __________________________________________________________
-| Input: 111  11                                           |
-| Output: 1010                                             |
-|__________________________________________________________|
-*/
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<string.h>
-#include<limits.h>
-#define SIZE 1000
+#include <bits/stdc++.h>
+#define N 100
+#define ll long long
+#define pp pair<int, int>
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define reset(x) memset(x, 0, sizeof(x))
+#define quyen_sort(x, size) sort(x + 1, x + 1 + size);
+using namespace std;
 
-void ex4(char s1[], char s2[]){
-	//your codes here
+void ex04(string s1, string s2)
+{
+    reverse(s1.begin(), s1.end());
+    reverse(s2.begin(), s2.end());
+    vector<int> res;
+    int debt = 0, x, y;
+    for (int i = 0; i < max(s1.length(), s2.length()) + 1; i++)
+    {
+        x = y = 0;
+        if (i < s1.length())
+            x = s1[i] - 48;
+        if (i < s2.length())
+            y = s2[i] - 48;
+        res.push_back(((x + y + debt) % 2));
+        debt = (x + y + debt) / 2;
+    }
+    for (int i = res.size() - 1; i >= 0; i--)
+        cout << char(res[i] + 48);
 }
+int main(int argc, char *argv[])
+{
+    string n1 = "111";
+    string n2 = "1111";
+    ex04(n1, n2);
 
-int main(int argc, char *argv[]) {
-	char n1[] = "111";
-    char n2[] = "1111";
-    ex4(n1,n2);
-		
-	return 0;
+    return 0;
 }
