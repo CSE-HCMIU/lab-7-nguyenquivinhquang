119ce053d90a7141420b2ce66a497214a17773f4
Nguyễn Quí Vinh Quang <60070577+nguyenquivinhquang@users.noreply.github.com>
2020-08-12 12:08:57 +0700

    Update Exercise5.cpp

--- Exercise5.cpp
+++ Exercise5.cpp
@@ -1,27 +1,33 @@
-/*
-5. Consider a T9 phone keypad layout (i.e. numeric key 2 is also used to input a, b or c),
-1 and 0 are considered empty characters. Write a program to input a series of keypads pressed
-then output the possible combinations of letters.
-Ex:
- ______________________________________
-| Input: 36                            |
-| Output: dm dn do em en eo fm fn fo   |
-|______________________________________|
-*/
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<string.h>
-#include<limits.h>
-#define SIZE 1000
-
-void ex5(char s[]){
-	//your codes here
+#include <bits/stdc++.h>
+#define N 100
+#define ll long long
+#define pp pair<int, int>
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define reset(x) memset(x, 0, sizeof(x))
+#define quyen_sort(x, size) sort(x + 1, x + 1 + size);
+using namespace std;
+string dict[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+char res[100];
+void ex5(string k, int depth = 0)
+{
+    if (depth > k.length())
+    {
+        for (int i = 1; i < depth; i++)
+            cout << res[i];
+        cout << " ";
+        return;
+    }
+    string temp = dict[int(k[depth - 1]) - '0'];
+    for (int i = 0; i < temp.length(); i++)
+    {
+        res[depth] = temp[i];
+        ex5(k, depth + 1);
+    }
 }
 
-int main(int argc, char *argv[]) {
-	char k[]="29";
-    ex5(k);
-	return 0;
+int main(int argc, char *argv[])
+{
+    string k = "29";
+    ex5(k, 1);
+    return 0;
 }
