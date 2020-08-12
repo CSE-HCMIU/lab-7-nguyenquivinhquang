8295da8cbaddddafb47f7f76d79ff0f6db716aaf
Nguyễn Quí Vinh Quang <60070577+nguyenquivinhquang@users.noreply.github.com>
2020-08-12 12:15:07 +0700

    Update Exercise2.cpp

--- Exercise2.cpp
+++ Exercise2.cpp
@@ -1,28 +1,23 @@
-/*
-2. Remove duplicate elements from an array and return the new length.
-Do not create any temporary array
-Ex:
- _________________________________________
-| Input: [ab, ac, ab, bc]                 |
-| Output: 3                               |
-|_________________________________________|
-*/
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<string.h>
-#include<limits.h>
-#define SIZE 1000
-
-void ex2(char *str[],int n){
-	//your codes here
+#include <bits/stdc++.h>
+#define N 100
+#define ll long long
+#define pp pair<int, int>
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL);
+#define reset(x) memset(x, 0, sizeof(x))
+#define quyen_sort(x, size) sort(x + 1, x + 1 + size);
+using namespace std;
+void ex2(char *str[], int n)
+{
+    map<string, int> trace;
+    for (int i = 0; i < n; i++)
+        trace[str[i]]++;
+    cout << trace.size();
 }
 
-int main(int argc, char *argv[]) {
-	
-	char *str[]={"ab","abcd","ab","abdr"};
-    ex2(str,4);
-		
-	return 0;
+int main(int argc, char *argv[])
+{
+    char *str[] = {"ab", "abcd", "ab", "abdr"};
+    ex2(str, 4);
+
+    return 0;
 }
